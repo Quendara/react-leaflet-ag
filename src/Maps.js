f530fb2b85626ccf98395cf40d3c489808a17b74
Quendara <andregarstka@web.de>
2020-03-18 15:08:55 +0100

    2 files added, 1 deleted

--- src/Maps.js
+++ src/Maps.js
@@ -7,6 +7,9 @@ import MarkerClusterGroup from "react-leaflet-markercluster";
 import { TourMockData } from "./tourdata";
 import { TourMockData2 } from "./tourdata";
 
+
+import {toGeoJSON}  from "./toGeoJSON"
+
 import "react-leaflet-markercluster/dist/styles.min.css";
 
 // type State = {
@@ -19,12 +22,20 @@ import "react-leaflet-markercluster/dist/styles.min.css";
 
 const convertGeoJsonToTrack = geoJSON => {
   const coords = geoJSON.features[0].geometry.coordinates;
-  const track = [];
+  const tourName = geoJSON.features[0].properties.name;
+  const time = geoJSON.features[0].properties.time;
+
+  const track = {
+    name: tourName,
+    time: time,
+    trackCoords: [],
+    color: "green"
+  };
 
   coords.map((item, index) => {
-
-    if (index % 4 === 0) {  // reduce data
-      track.push([item[1], item[0]]);
+    if (index % 4 === 0) {
+      // reduce data
+      track.trackCoords.push([item[1], item[0]]);
     }
 
     if (index % 500 === 0) {
@@ -56,15 +67,21 @@ class SimpleMap extends Component {
 
     this.state = {
       tracks: [],
-      markerArr : [], // getRandPositions(10, position);
+      markerArr: [], // getRandPositions(10, position);
       centerPosition: [],
       zoom: 11
     };
 
-    this.state.tracks.push(convertGeoJsonToTrack(TourMockData));
-    this.state.tracks.push(convertGeoJsonToTrack(TourMockData2)); 
 
-    this.state.centerPosition = this.state.tracks[0][0]
+
+    this.state.tracks.push(convertGeoJsonToTrack(TourMockData));
+    this.state.tracks.push(convertGeoJsonToTrack(TourMockData2));
+    // this.state.tracks.push(convertGeoJsonToTrack(TourMockData3));
+
+    this.state.tracks[1].color = "darkblue"
+
+    // get center of the
+    this.state.centerPosition = this.state.tracks[0].trackCoords[0];
   }
 
   render() {
@@ -88,7 +105,19 @@ class SimpleMap extends Component {
         ))}
 
         {this.state.tracks.map((track, index) => (
-          <Polyline color="green" positions={track} />
+          <>
+            <Marker position={track.trackCoords[0]}>
+              <Popup>
+                <b>Tour</b> <br/> {track.name}
+                <hr />
+                {track.time}
+                <hr />
+                {track.trackCoords[0]}, {track.trackCoords[1]}
+              </Popup>
+            </Marker>
+
+            <Polyline color={track.color} positions={track.trackCoords} />
+          </>
         ))}
       </Map>
     );
